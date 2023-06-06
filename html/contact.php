c88e1ccadf7497a7ecdaa072dde8cd4f34658403
brianvanhetgrafischlyceum <120646159+brianvanhetgrafischlyceum@users.noreply.github.com>
2023-06-06 17:08:40 +0200

    problemen gefixt
    fouten in het formulier en php zijn gefixt waardoor de pagina nu wel werkt

--- html/contact.php
+++ html/contact.php
@@ -4,35 +4,14 @@ $databaseFile = "mijnvilla.db";
 // Verbinding maken met de database
 try {
     $dbh = new PDO("sqlite:" . $databaseFile);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Fout bij het maken van de databaseverbinding: " . $e->getMessage();
     die();
 }
-
-// Controleer of het formulier is ingediend
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Gegevens van het formulier ophalen
-    $voornaam = $_POST['voornaam'];
-    $achternaam = $_POST['achternaam'];
-    $email = $_POST['email'];
-    $telefoonnummer = $_POST['telefoonnummer'];
-    $vraag_opmerking = $_POST['vraag_opmerking'];
-
-    // Voeg gegevens toe aan de database
-    $query = "INSERT INTO contact_form (voornaam, achternaam, email, telefoonnummer, vraag_opmerking) 
-              VALUES (?, ?, ?, ?, ?)";
-    $statement = $dbh->prepare($query);
-    $statement->execute([$voornaam, $achternaam, $email, $telefoonnummer, $vraag_opmerking]);
-
-    echo "Opmerking geplaatst" ;
-}
-
-// Vraag/opmerkingen uit de database ophalen
-$query = "SELECT voornaam, achternaam, vraag_opmerking FROM contact_form";
-$statement = $dbh->query($query);
-$vraag_opmerkingen = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
+
+
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -62,24 +41,58 @@ $vraag_opmerkingen = $statement->fetchAll(PDO::FETCH_ASSOC);
         <button id="register-btn">Register</button>
       </div>
     </nav>
-    <form action="/php/contact.php method="POST" id="contact-form">
-      <h1>Neem contact met ons op</h1>
-      <p>
-        heeft u een vraag of is er iets waarvan u denkt: dit klopt niet? neem
-        dan contact met ons op via dit formulier
-      </p>
-      <div id="form">
-        <input type="text" placeholder="voornaam" />
-        <input type="text" placeholder="achternaam" />
-        <input type="text" placeholder="telefoonnummer" />
-        <input type="text" placeholder="email" />
-        <textarea placeholder="vraag/opmerking" cols="40" rows="3"></textarea>
-        <input type="submit" value="verstuur" class="submit-button" />
-      </div>
-    </form>
+    <form action="" method="POST" id="contact-form">
+  <h1>Neem contact met ons op</h1>
+  <p>
+    heeft u een vraag of is er iets waarvan u denkt: dit klopt niet? neem
+    dan contact met ons op via dit formulier
+  </p>
+  <div id="form">
+    <input type="text" name="voornaam" placeholder="voornaam" />
+    <input type="text" name="achternaam" placeholder="achternaam" />
+    <input type="text" name="telefoonnummer" placeholder="telefoonnummer" />
+    <input type="text" name="email" placeholder="email" />
+    <textarea name="vraag_opmerking" placeholder="vraag/opmerking" cols="40" rows="3"></textarea>
+    <input type="submit" value="verstuur" class="submit-button" />
+  </div>
+  <?php
+// Controleer of het formulier is ingediend
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // Gegevens van het formulier ophalen
+  $voornaam = $_POST['voornaam'];
+  $achternaam = $_POST['achternaam'];
+  $email = $_POST['email'];
+  $telefoonnummer = $_POST['telefoonnummer'];
+  $vraag_opmerking = $_POST['vraag_opmerking'];
 
+  // Controleren of de email al bestaat in de database
+  $existingQuery = "SELECT COUNT(*) as count FROM contact_form WHERE email = ?";
+  $existingStatement = $dbh->prepare($existingQuery);
+  $existingStatement->execute([$email]);
+  $existingResult = $existingStatement->fetch(PDO::FETCH_ASSOC);
+
+  if ($existingResult['count'] > 0) {
+      echo "Een opmerking met dit e-mailadres bestaat al.";
+  } else {
+      // Voeg gegevens toe aan de database
+      $insertQuery = "INSERT INTO contact_form (voornaam, achternaam, email, telefoonnummer, vraag_opmerking) 
+                      VALUES (?, ?, ?, ?, ?)";
+      $insertStatement = $dbh->prepare($insertQuery);
+      $insertStatement->execute([$voornaam, $achternaam, $email, $telefoonnummer, $vraag_opmerking]);
+
+}
+}
+
+// Vraag/opmerkingen uit de database ophalen
+$query = "SELECT voornaam, achternaam, vraag_opmerking FROM contact_form";
+$statement = $dbh->query($query);
+$vraag_opmerkingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+</form>
+    <div class="opmerkingen">
     <?php
-foreach ($vraag_opmerkingen as $vraag_opmerking) {
+    foreach ($vraag_opmerkingen as $vraag_opmerking) {
     $voornaam = $vraag_opmerking['voornaam'];
     $achternaam = $vraag_opmerking['achternaam'];
     $bericht = $vraag_opmerking['vraag_opmerking'];
@@ -87,6 +100,7 @@ foreach ($vraag_opmerkingen as $vraag_opmerking) {
     echo "<p><strong>$voornaam $achternaam:</strong> $bericht</p>";
 }
 ?>
+    </div>
 
     <footer>
       <div id="footer-nav">
